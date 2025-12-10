5e13453bfc34232b22efb48637f10920125470e5
vossibaby <itsvossibaby@gmail.com>
2025-12-10 21:30:21 +0500

    button text

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -1,4 +1,8 @@
-export const Button = ({ children = "Back", className = "", ...props }) => (
+export const Button = ({
+  children = "Back to Home",
+  className = "",
+  ...props
+}) => (
   <button
     type="button"
     className={`text-white bg-dark-100 box-border border border-transparent hover:bg-[#010a83]  focus:ring-2 focus:ring-neutral-tertiary shadow-xs font-medium leading-5 rounded-xl text-sm px-4 py-2.5 focus:outline-none ${className}`}
